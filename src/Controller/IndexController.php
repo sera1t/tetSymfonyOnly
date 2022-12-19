bc628f543a4bf14b8e9cadf3bd625012d34117cf
root <root@sera1t.sera1t>
2022-12-19 07:42:32 -0600

    Add controller

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    #[Route('/', name: 'app_index')]
+    public function index(): Response
+    {
+        return new Response(
+            <<<EOF
+            <html>
+                <body>
+                    
+                </body>
+            </html>
+            EOF
+        );
+    }
+}
